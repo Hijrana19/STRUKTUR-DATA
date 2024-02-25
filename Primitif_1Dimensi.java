a8ebaa699fd6f4203b63a9550e4162ccd85299ea
HIJRANA <100138786+Hijrana19@users.noreply.github.com>
2024-02-25 13:14:50 +0800

    Add files via upload

---
+++ Primitif_1Dimensi.java
@@ -0,0 +1,47 @@
+package Projek;
+
+public class Primitif_1Dimensi {
+    public static void main(String[] args) {
+        //1. Tipe data Integer
+        int [] nilaiint = {10,20,30,40,50};
+        System.out.println("Panjang array integer:" + nilaiint.length);
+        System.out.println("Nilai Pertama:" + nilaiint[0]);
+        
+        //2. Tipe data Float
+        float [] nilaifloat = {12.5f, 30.2f, 20.12f};
+        System.out.println("Panjang Array Float:" + nilaifloat.length);
+        System.out.println("Nilai Kedua:" + nilaifloat[1]);
+        
+        //3. Tipe data Byte
+        byte [] nilaibyte = {-29, 21, 22, -30};
+        System.out.println("Panjang Array Byte:" + nilaibyte.length);
+        System.out.println("Nilai Pertama:" + nilaibyte[0]);
+        
+        //4. Tipe Data Short
+        short [] nilaishort = {2021, 2022, 2023, 2024};
+        System.out.println("Panjang Array Short:" + nilaishort.length);
+        System.out.println("Nilai Pertama:" + nilaishort[0]);
+        
+        //5. Tipe Data Long
+        long [] nilailong = {199928, 2901929, 2910339};
+        System.out.println("Panjang Array Long:" + nilailong.length);
+        System.out.println("Nilai Pertama:" + nilailong[0]);
+        
+        //6. Tipe Data Double
+        double [] nilaidouble = {3.14, 0.0, 3.1};
+        System.out.println("Panjang Array Double:" + nilaidouble.length);
+        System.out.println("Nilai Pertama:" + nilaidouble[0]);
+        
+        //7. Tipe Data Char
+        char [] nilaichar = {'A','B','C','+'};
+        System.out.println("Panjang Array Char:" + nilaichar.length);
+        System.out.println("Nilai Pertama:" + nilaichar[0]);
+        
+        //8. Tipe Data Boolean
+        boolean [] nilaibool = {false,true};
+        System.out.println("Panjang Array Boolean:" + nilaibool.length);
+        System.out.println("Nilai Pertama:" + nilaibool[0]);
+        
+    }
+    
+}
